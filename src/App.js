e58c4c11e29d6a6d44219e47d310d7a6109c3d02
lrudkova <lyudmila.rudkova@t-systems.com>
2020-05-11 13:58:08 +0300

    Update main message

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          kok there
+          I love hinkali!
         </p>
         <a
           className="App-link"
